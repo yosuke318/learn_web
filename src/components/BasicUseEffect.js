08a2fa8b154c97ed7b1c60c5b671325761b43869
Yosuke Maruyama <youman.318@gmail.com>
2022-04-04 21:30:50 +0900

    Create a mechanism to respond to clicks using useEffect

--- src/components/BasicUseEffect.js
+++ src/components/BasicUseEffect.js
@@ -2,14 +2,17 @@ import React,{useState, useEffect} from 'react'
 
 const BasicUseEffect = () => {
     const [count, setCount] = useState(0)
+    const [item, setItem] = useState('')
+    
     // renderingされた最初の一回だけ呼ばれる
     useEffect(()=> {
         console.log('useEffect invoke')
-    },[]
+    },[count]
     )
     return (
         <div>
             <button onClick={()=> setCount(prevCount=>prevCount+1)}>Click {count}</button>
+            <input type='text' value={item} onChange={evt=>setItem(evt.target.value)}/>
         </div>
     )
 }
