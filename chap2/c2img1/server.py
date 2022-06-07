c267391cf29362e1252cacfe90f9ffe3df5241e7
yosuke318 <youman.318@gmail.com>
2022-06-07 23:02:22 +0900

    run flask server in docker container

---
+++ chap2/c2img1/server.py
@@ -0,0 +1,14 @@
+import flask
+app = flask.Flask('app server')
+
+@app.route('/')
+def index():
+    return 'Hello Docker'
+
+app.run(debug=True, host='0.0.0.0', port=80)
+
+# 立ち上げたコンテナにコピーするファイル
+# docker container run --name base -it-p 8080:80 python bashでコンテナ作成
+# コンテナに入りpip install flask ==2.n.nでflaskをインストール
+# docker container cp コピー元 コンテナ名:コンテナ内のパス
+# コンテナに再び入りpython -u /server.pyを実行
