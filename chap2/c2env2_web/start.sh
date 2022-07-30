3aff14843c68fa47d7bc39cdb5ace55709a9c950
yosuke318 <youman.318@gmail.com>
2022-07-30 21:43:22 +0900

    wip :shが実行できない

--- chap2/c2env2_web/start.sh
+++ chap2/c2env2_web/start.sh
@@ -1,3 +1,7 @@
 sed -e "s/{{PORT}}/$PORT/g" /etc/nginx/nginx.tpl > /etc/nginx/nginx.conf
 sed -i -e "s^{{APP_SERVER}}^$APP_SERVER^g" /etc/nginx/nginx.conf
 exec nginx -g "daemon off;"
+
+# アプリコンテナへ転送する
+# docker container exec c2env_app hostname -i apサーバのIP確認
+# 転送できない
