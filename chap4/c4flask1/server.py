4430b338ce8cc7073c10c3f01550e7be91a8e1cb
yosuke318 <yosuke.maruyama@hightec.co.jp>
2022-12-17 22:33:16 +0900

    feature : c4 run flask app test and study nocache

--- chap4/c4flask1/server.py
+++ chap4/c4flask1/server.py
@@ -1,7 +1,8 @@
 import os, flask
 PORT = int(os.environ['PORT'])
 app = flask.Flask('app server')
-@app.route('/')
+@app.route('/') #localhost:8080
+# @app.route('/api/v1/hello')# http://localhost:8080/api/v1/hello
 def index():
     return 'hello Dockerfile'
 app.run(debug=True, host='0.0.0.0', port=PORT)
