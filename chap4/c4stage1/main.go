57eb2a5e6c4af76615d6746d1bbe61a746b3c8f7
yosuke318 <yosuke.maruyama@hightec.co.jp>
2022-12-17 23:51:59 +0900

    feature : create binary file from main.go by Dockerfile

--- chap4/c4stage1/main.go
+++ chap4/c4stage1/main.go
@@ -1,14 +1,14 @@
 package main
 
 import(
-    "net/http"
-    "fmt"
+  "net/http"
+  "fmt"
 )
 
 func main(){
-    http.HandlerFunc("/",
-        func(w http.ResponseWriter, r *http.Request){
-            fmt.Fprintf(w, "hello go")
-        })
-    http.ListenAndServe(":80", nil)
+  http.HandleFunc("/",
+    func(w http.ResponseWriter, r *http.Request){
+      fmt.Fprintf(w, "hello go")
+    })
+  http.ListenAndServe(":80", nil)
 }
