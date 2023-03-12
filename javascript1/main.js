92e9abfc657d9a6e4b202c61f74b67f2a989ea99
yosuke_santamaria318 <yosuke.maruyama@hightec.co.jp>
2023-03-12 11:29:32 +0900

    javascript timer start and stop

--- javascript1/main.js
+++ javascript1/main.js
@@ -1,11 +1,28 @@
+var displayElm = document.getElementsByClassName('display')[0];
 var startButton = document.getElementsByClassName('startButton')[0];
+var timer = null;
 
+// addEventListenerはクリックなどのイベントが発生した時に発火する関数
 startButton.addEventListener('click', function () {
-    console.log('start');
-    var seconds = 0;
-    //https://developer.mozilla.org/ja/docs/Web/API/setInterval
-    setInterval(function () {
-        seconds++;
-        console.log(seconds);
-    }, 1000);
+    if (timer === null) {
+        console.log('start');
+        var seconds = 0;
+
+        //https://developer.mozilla.org/ja/docs/Web/API/setInterval
+        timer = setInterval(function () {
+            seconds++;
+            miliseconds = seconds * 0.1;
+            miliseconds = parseFloat(miliseconds.toFixed(1));
+            displayElm.innerText = miliseconds;
+            console.log(miliseconds);
+        }, 100);
+    }
 });
+
+var stopButton = document.getElementsByClassName('stopButton')[0];
+stopButton.addEventListener('click', function () {
+    if (timer !== null) {
+        clearInterval(timer);
+        timer = null;
+    }
+})
