c23a6e548e8d33737f82fb54b126b92278410075
yosuke_santamaria318 <yosuke.maruyama@hightec.co.jp>
2023-03-12 10:48:20 +0900

    step1 count seconds

--- javascript1/main.js
+++ javascript1/main.js
@@ -1,36 +1,11 @@
-console.log('ファイル読み込みのテストです');
-var element = document.getElementById('innerTest')
-element.innerHTML = '<strong>JavaScriptで書きました</strong>'
+var startButton = document.getElementsByClassName('startButton')[0];
 
-var buttonElm = document.getElementById('testButton');
-buttonElm.addEventListener('click', function () {
-
-  var numberElm = document.getElementById('number')
-  var val = numberElm.value;
-  var num = parseInt(val);
-  if (num % 2 == 0) {
-    alert('偶数です');
-  } else {
-    alert('偶数ではない')
-  }
+startButton.addEventListener('click', function () {
+    console.log('start');
+    var seconds = 0;
+    //https://developer.mozilla.org/ja/docs/Web/API/setInterval
+    setInterval(function () {
+        seconds++;
+        console.log(seconds);
+    }, 1000);
 });
-
-var fruits = ["apple", "peach", "orange"]
-var fruitsStr = '';
-for(var i = 0; i< fruits.length; i++) {
-  fruitsStr += '<li class="fruit">' + fruits[i] + '</li>';
-}
-
-var arrayElm = document.getElementById('arrayTest');
-arrayElm.innerHTML = fruitsStr;
-
-// id属性と違いclass属性は画面に複数配置することができるため、class属性の名前を条件に指定すると複数の要素が取れる可能性があります。
-var fruitElms = document.getElementsByClassName('fruit');
-for(var i = 0; i < fruitElms.length; i++){
-  var fruitElm = fruitElms[i];
-  console.log(fruitElm.textContent);
-}
-
-// 要素のみを取り出すこともできるべ
-var fruitElms = document.getElementsByClassName('fruit')[0];
-console.log(fruitElms.innerHTML)
