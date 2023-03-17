001df546dc8fd08e320ca16866f9fb9d7818ec07
yosuke_santamaria318 <yosuke.maruyama@hightec.co.jp>
2023-03-17 21:21:53 +0900

    display 開始

--- javascript1/main.js
+++ javascript1/main.js
@@ -4,6 +4,7 @@ var timer = null;
 
 // addEventListenerはクリックなどのイベントが発生した時に発火する関数
 startButton.addEventListener('click', function () {
+    // ボタンがクリックされて、timer変数がnullなら発火
     if (timer === null) {
         console.log('start');
         var seconds = 0;
@@ -16,11 +17,26 @@ startButton.addEventListener('click', function () {
             displayElm.innerText = miliseconds;
             console.log(miliseconds);
         }, 100);
+
+        var message = '開始';
+
+        // document.createElementは、指定した名前のHTMLタグ要素を作成することができる関数(今回はdiv)
+        var messageElm = document.createElement('div')
+
+        // 開始という文字をタグ内に表示する。
+        messageElm.innerText = message
+
+        // document.querySelectorは、引数に与えたCSSセレクタに合致する画面上のHTMLタグ要素を一つ取得する.
+        var logElm = document.querySelector('.log');
+
+        // 取得したHTMLタグ要素の子としてmessageElm(作成したdivタグ)を描画する
+        logElm.appendChild(messageElm);
     }
 });
 
 var stopButton = document.getElementsByClassName('stopButton')[0];
 stopButton.addEventListener('click', function () {
+    // ボタンがクリックされて、timer変数がnullでないなら発火
     if (timer !== null) {
         clearInterval(timer);
         timer = null;
